34ef57c54714f1bb7e2f4515528f29da1bd576a3
Geno Erickson <gerickson4@wi.rr.com>
2016-01-25 22:30:08 -0600

    Version 1.0 working

---
+++ Pods/Target Support Files/Pods-SuctionPeach-Showcase/Pods-SuctionPeach-Showcase-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SuctionPeach_Showcase : NSObject
+@end
+@implementation PodsDummy_Pods_SuctionPeach_Showcase
+@end
---
+++ Pods/Target Support Files/Pods-SuctionPeach-Showcase/Pods-SuctionPeach-Showcase-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SuctionPeach_ShowcaseVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SuctionPeach_ShowcaseVersionString[];
+
